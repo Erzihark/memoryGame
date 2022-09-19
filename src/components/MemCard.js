3920ba3f7cfa1eed88bfee0918512cd295fae277
ManuelCarretero <manuel.carretero@undostres.com.mx>
2022-09-18 20:54:46 -0500

    fix(memory): Flipped cards back (lol)

--- src/components/MemCard.js
+++ src/components/MemCard.js
@@ -6,7 +6,7 @@ import '../index.css';
 export default function MemCard({img, id, pos, cardArr, flipCard}){
 
     return(
-        <Card id={id} pos={pos} onClick={(e) => flipCard(e, id)} className={cardArr[pos] > 0 ? 'face-up memcard' : 'face-down memcard'}>
+        <Card id={id} pos={pos} onClick={(e) => flipCard(e, id)} className={cardArr[pos] < 0 ? 'face-up memcard' : 'face-down memcard'}>
             <Card.Img src={"%PUBLIC_URL%/" + img}/>
             <Card.Body>
                     <span className='card-mark'>?</span>
